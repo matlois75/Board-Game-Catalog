0340a54018c2f944e0ba31a0d60ced336ac8372a
matlois75 <mathys.loiselle@gmail.com>
2024-09-13 11:48:27 -0700

    Fixed firebase initialization (again)

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 const firebaseConfig = {
   apiKey: 'FIREBASE_API_KEY',
   authDomain: 'FIREBASE_AUTH_DOMAIN',
+  databaseURL: 'FIREBASE_DATABASE_URL',
   projectId: 'FIREBASE_PROJECT_ID',
   storageBucket: 'FIREBASE_STORAGE_BUCKET',
   messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
-  appId: 'FIREBASE_APP_ID'
+  appId: 'FIREBASE_APP_ID',
+  measurementId: 'FIREBASE_MEASUREMENT_ID'
 };
 
 const DEEPL_API_KEY = 'DEEPL_API_KEY';
